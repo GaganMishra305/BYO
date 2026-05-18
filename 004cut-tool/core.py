5ca86e5003f316dd84b17e2d51414b72f183d693
Gagan Mishra <simonsimple305@gmail.com>
2026-05-18 21:57:41 +0530

    completed step2 for the byo cut tool

--- 004cut-tool/core.py
+++ 004cut-tool/core.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 import sys
 
-def cut(filename, field):
+def cut(filename, field, deli):
     with open(filename) as f:
         for line in f:
-            parts = line.rstrip('\n').split('\t')
+            parts = line.rstrip('\n').split(deli)
             idx = field - 1
             print(parts[idx] if idx < len(parts) else '')
 
 if __name__ == '__main__':
-    # parse -f2 style arg
     field = 1
     files = []
+    deli = "\t"
     for arg in sys.argv[1:]:
         if arg.startswith('-f'):
             field = int(arg[2:])
+        elif arg.startswith("-d"):
+            deli = arg[2:]
         else:
             files.append(arg)
     
     for f in files:
-        cut(f, field)
+        cut(f, field, deli)
