af0ecef2579f8acfb9649c3802d46ad898f71163
Gagan Mishra <simonsimple305@gmail.com>
2026-05-23 03:41:51 +0530

    refactor - cut tool built complete

--- 004cut-tool/core.py
+++ 004cut-tool/core.py
@@ -15,27 +15,26 @@ def cut(filename, fields, deli):
     with open(filename) as f:
         process_stream(f, fields, deli)
 
-if __name__ == '__main__':
-    fields = [1]
-    files = []
-    deli = "\t"
-    for arg in sys.argv[1:]:
-        if arg.startswith('-f'):
-            fields_str = arg[2:]
-            if fields_str.count(","):
-                fields = [int(f) for f in fields_str.split(',')]
-            elif fields_str.count(" "):
-                fields = [int(f) for f in fields_str.split(' ')]
-        elif arg.startswith("-d"):
-            deli = arg[2:]
-        else:
-            files.append(arg)
-    
-    if not files or files == ['-']:
-        process_stream(sys.stdin, fields, deli)
+fields = [1]
+files = []
+deli = "\t"
+for arg in sys.argv[1:]:
+    if arg.startswith('-f'):
+        fields_str = arg[2:]
+        if fields_str.count(","):
+            fields = [int(f) for f in fields_str.split(',')]
+        elif fields_str.count(" "):
+            fields = [int(f) for f in fields_str.split(' ')]
+    elif arg.startswith("-d"):
+        deli = arg[2:]
     else:
-        for f in files:
-            if f == "-":
-                process_stream(sys.stdin, fields, deli)
-            else:
-                cut(f, fields, deli)
+        files.append(arg)
+
+if not files or files == ['-']:
+    process_stream(sys.stdin, fields, deli)
+else:
+    for f in files:
+        if f == "-":
+            process_stream(sys.stdin, fields, deli)
+        else:
+            cut(f, fields, deli)
