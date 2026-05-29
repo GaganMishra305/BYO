4656db86dd87165dd1d1a09e8d1625304dff9deb
Gagan Mishra <simonsimple305@gmail.com>
2026-05-29 22:19:15 +0530

    loadbalancer completed

--- 005load-balancer/main.go
+++ 005load-balancer/main.go
@@ -6,29 +6,43 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"strconv"
 )
 
 func main() {
-	backendURL, err := url.Parse("http://localhost:10000")
-	if err != nil {
-		log.Fatalf("Failed to parse backend URL: %v", err)
+	// time.Sleep(1 * time.Second) // wait for 2 seconds for all the servers to startup
+	num := os.Args[1]
+	tot, _ := strconv.Atoi(num)
+	fmt.Printf("Numbers of servers successfully started = " + num + "\n")
+	fmt.Printf("Started load balancer on 8080\n")
+
+	var backends []*url.URL
+	for i := 0; i < tot; i++ {
+		port := 10000 + i
+		backendURL, err := url.Parse("http://localhost:" + strconv.Itoa(port))
+		if err != nil {
+			log.Fatalf("Failed to parse backend URL: %v", err)
+		}
+		backends = append(backends, backendURL)
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(backendURL)
-
+	cur := -1
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cur = (cur + 1) % tot
+		backendURL := backends[cur]
+		proxy := httputil.NewSingleHostReverseProxy(backendURL)
+
 		// Log the incoming request
 		requestDump, err := httputil.DumpRequest(r, true)
 		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Println("Received request from", r.RemoteAddr)
+		fmt.Println("Forwarding to", backendURL)
 		fmt.Println(string(requestDump))
-
-		// Forward the request to the backend
 		proxy.ServeHTTP(w, r)
 	})
 
-	fmt.Println("Load balancer is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- 005load-balancer/run.sh
+++ 005load-balancer/run.sh
@@ -1,13 +1,18 @@
+trap 'kill $(jobs -p)' EXIT
+
 N=${1:-5}
 # run up N servers on port 10000-1000(N-1)
 echo "Starting $N servers on ports 10000-1000$((N-1))"
 cd backend
+SN=0
 for i in $(seq 0 $((N-1))); do
     port=$((10000 + i))
     go run main.go $port &
+    if [ $? -eq 0 ]; then
+        SN=$((SN+1))
+    fi
 done
 
 # run up the load balancer on port 5000
-echo "Starting load balancer on port 5000"
 cd ..
-go run main.go
+go run main.go $SN
